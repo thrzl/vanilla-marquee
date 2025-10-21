eb1f19223cfcf16c5e197263784ac228935609e8
te <thrzl@icloud.com>
2025-10-20 21:41:56 -0400

    use biome instead of eslint

--- rollup.config.js
+++ rollup.config.js
@@ -1,56 +1,55 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
-import eslint from '@rollup/plugin-eslint';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
 
 export default [
   // Custom per tutte le pagine
   {
-    input: 'src/vanilla-marquee.js',
-    output: [{
-      plugins: [
-        terser({
-          mangle: {
-            properties: {
-              regex: /^_*/,
-              reserved: [
-                'css3easing',
-                'delayBeforeStart',
-                'direction',
-                'duplicated',
-                'duration',
-                'gap',
-                'pauseOnHover',
-                'recalcResize',
-                'speed',
-                'startVisible',
-              ]
+    input: "src/vanilla-marquee.js",
+    output: [
+      {
+        plugins: [
+          terser({
+            mangle: {
+              properties: {
+                regex: /^_*/,
+                reserved: [
+                  "css3easing",
+                  "delayBeforeStart",
+                  "direction",
+                  "duplicated",
+                  "duration",
+                  "gap",
+                  "pauseOnHover",
+                  "recalcResize",
+                  "speed",
+                  "startVisible",
+                ],
+              },
             },
-          },
-        }),
-      ],
-      dir:            'dist/',
-      format:         'es',
-      indent:         false,
-      entryFileNames: '[name].min.js',
-    }, {
-      dir:            'dist/',
-      format:         'es',
-      indent:         false,
-      entryFileNames: '[name].js',
-    }],
-    context: 'window',
+          }),
+        ],
+        dir: "dist/",
+        format: "es",
+        indent: false,
+        entryFileNames: "[name].min.js",
+      },
+      {
+        dir: "dist/",
+        format: "es",
+        indent: false,
+        entryFileNames: "[name].js",
+      },
+    ],
+    context: "window",
     plugins: [
-      eslint(),
       resolve({
-        browser:        true,
-        modules:        true,
+        browser: true,
+        modules: true,
         preferBuiltins: false,
       }),
       commonjs({
-        include: [
-          /node_modules/,
-        ], // Default: undefined
+        include: [/node_modules/], // Default: undefined
         sourceMap: false,
       }),
     ],
